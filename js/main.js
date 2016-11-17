868b27bcae0c47f576d2b20e0718c0988bbe4e34
JC <jonathan.david.clare@gmail.com>
2016-11-17 16:51:11 +0000

    Added facebook feed

--- js/main.js
+++ js/main.js
@@ -2,28 +2,57 @@ window.onload = function ()
 { 
     lazyLoad();
     on(window, 'scroll resize', debounce(lazyLoad));
-    // Twitter feed.
-    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-} 
+ } 
 
 // Remove filename from location.
-history.replaceState({}, document.title, ".");
+history.replaceState({}, document.title, '.');
+var twitterLoaded = false;
+var fbLoaded = false;
 
 function lazyLoad () 
 {
-    console.log('lazyLoad');
+    // Images.
+    var offset = pageOffset().y
+    var pageBottom = offset + viewportHeight();
     [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) 
     {
-        var sb = pageOffset().y + viewportHeight();
-        if(bounds(img).top < sb)
+        var imgTop = offset + bounds(img).top;
+        if(imgTop <= pageBottom)
         {
             img.setAttribute('src', img.getAttribute('data-src'));
-            img.onload = function()  
-            {
-                img.removeAttribute('data-src');
-            }
+            img.onload = function() {img.removeAttribute('data-src');}
         }
     });
+
+    // Twitter feed.
+    if (!twitterLoaded)
+    {
+        var twitter = document.querySelector('.twitter');
+        var twitterTop = offset + bounds(twitter).top;
+        if(twitterTop <= pageBottom)
+        {   
+            twitterLoaded = true;
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+        }
+    }
+
+    // fb feed.
+    if (!fbLoaded)
+    {
+        var fb = document.querySelector('.twitter');
+        var fbTop = offset + bounds(fb).top;
+        if(fbTop <= pageBottom)
+        {   
+            (function(d, s, id) 
+            {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        }
+    }
 }
 
 function openArea(area) 
