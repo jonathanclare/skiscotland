b6a99d0be5712e442631a8072edfa118872f573a
JC <jonathan.david.clare@gmail.com>
2016-11-17 15:46:31 +0000

    Remove filename from url

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 window.onload = function () 
 { 
     lazyLoad();
-    on(window, 'scroll resize', lazyLoad);
+    on(window, 'scroll resize', debounce(lazyLoad));
     // Twitter feed.
     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-}
+} 
 
-var count = 0;
+// Remove filename from location.
+history.replaceState({}, document.title, ".");
 
 function lazyLoad () 
 {
+    console.log('lazyLoad');
     [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) 
     {
         var sb = pageOffset().y + viewportHeight();
@@ -19,8 +21,6 @@ function lazyLoad ()
             img.onload = function()  
             {
                 img.removeAttribute('data-src');
-                count++;
-                document.querySelector(".count").innerHTML = count;
             }
         }
     });
@@ -59,3 +59,21 @@ function pageOffset()
         y : (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0)
     };
 }
+
+function debounce(func, wait, immediate) 
+{
+    var timeout;
+    return function() 
+    {
+        var me = this, args = arguments;
+        var later = function() 
+        {
+            timeout = null;
+            if (!immediate) func.apply(me, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait || 250);
+        if (callNow) func.apply(me, args);
+    };
+};
